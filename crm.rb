823f9901094b41d8d980b508563d88409335e026
wtung008 <wayne.h.tung@gmail.com>
2017-01-24 14:38:31 -0500

    add Contact.all method to contacts.erb

--- crm.rb
+++ crm.rb
@@ -2,7 +2,13 @@
 # Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
 # You'll have to implement it from scratch.
 
+require_relative 'contact'
 require 'sinatra'
+
+Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
+
 
 get '/' do
   @crm_app_name = "Bitmaker's CRM"
