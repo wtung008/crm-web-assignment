527bf1c681baaeb71dc9b100df31859058c5b27a
wtung008 <wayne.h.tung@gmail.com>
2017-01-24 15:30:22 -0500

    add params and redirect to crm.rb file

--- crm.rb
+++ crm.rb
@@ -5,9 +5,9 @@
 require_relative 'contact'
 require 'sinatra'
 
-Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
-Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
-Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
+#Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+#Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+#Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
 
 
 get '/' do
@@ -23,4 +23,9 @@ end
 
 get '/contacts/new' do
   erb :new_contact
+end
+
+post '/contacts' do
+  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+  redirect to('/contacts')
 end
