2358e6644acfded4147180e6901a458aa45f1a97
wtung008 <wayne.h.tung@gmail.com>
2017-01-26 01:40:09 -0500

    Add 404 Error message redirect to crm.rb file

--- crm.rb
+++ crm.rb
@@ -39,5 +39,9 @@ end
 
 get '/contacts/:id' do
   @contact = Contact.find(params[:id].to_i)
-  erb :show_contact
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
 end
